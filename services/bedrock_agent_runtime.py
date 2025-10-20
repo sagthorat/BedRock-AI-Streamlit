216817b5bd155701901be9f883cceed11d528bed
sagar.thorat <sagar.thorat@cenitex.vic.gov.au>
2025-10-20 11:43:12 +1100

    updated missing comma

--- services/bedrock_agent_runtime.py
+++ services/bedrock_agent_runtime.py
@@ -8,11 +8,12 @@ logger = logging.getLogger(__name__)
 
 def invoke_agent(agent_id, agent_alias_id, session_id, prompt):
     try:
-        client = boto3.session.Session().client(service_name="bedrock-agent-runtime", region_name='ap-southeast-2')               #1 Add Region Name
+        region = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
+        client = boto3.session.Session().client(service_name="bedrock-agent-runtime", region_name=region)
         # See https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/invoke_agent.html
         response = client.invoke_agent(
-            agentId=os.getenv('BEDROCK_AGENT_ID'),                     #2 Add Agent ID
-            agentAliasId=os.getenv('BEDROCK_AGENT_ALIAS_ID')                #3 Add Agent Alias ID
+            agentId=agent_id,
+            agentAliasId=agent_alias_id,
             enableTrace=True,
             sessionId=session_id,
             inputText=prompt
